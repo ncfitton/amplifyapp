2b62c1accb2db4c2b89f69e4a724acaef94f2a01
Nick Fitton <ncfitton@gmail.com>
2020-04-28 07:46:38 -0700

    Part 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import React from 'react';
        <header className="App-header">
          <img src={logo} className="App-logo" alt="logo" />
          <p>
-           Edit <code>src/App.js</code> and save to reload.
+           Edit <code>src/App.js</code> and save to reload Part 2.
          </p>
          <a
            className="App-link"
